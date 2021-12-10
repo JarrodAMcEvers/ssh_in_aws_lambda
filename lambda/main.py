a3161a47b048bc7de4d7e6c533657b5e0efe74f5
Jarrod McEvers <jarrodamcevers@gmail.com>
2021-12-10 14:33:21 -0600

    Just have one file -- all functions will use the same file. Use get git lambda from temp.

--- lambda/main.py
+++ lambda/main.py
@@ -1,4 +1,26 @@
 import os
+import boto3
+
+ID_RSA_PATH='/tmp/id_rsa'
+REPO_URL=os.getenv('REPO_TO_DOWNLOAD')
+REPO_PATH='/tmp/{}'.format(REPO_URL)
+
+secretsmanager = boto3.client('secretsmanager')
+
+def retrieve_and_save_ssh_key():
+    key = secretsmanager.get_secret_value(SecretId=os.getenv('SSH_KEY_SECRET_ID'))['SecretString']
+
+    with open(ID_RSA_PATH, 'w') as file:
+        file.write(key)
+        # without the newline, the private key will not be in the correct format
+        file.write('\n')
+    os.chmod(ID_RSA_PATH, 0o400)
 
 def handler(event, context):
+    # this line will fail for the lambda function that has the default ssh
     os.system('ssh -vT git@github.com')
+
+    retrieve_and_save_ssh_key()
+
+    os.system('git clone {} {}'.format(REPO_URL, REPO_PATH))
+    os.system('tree {}'.format(REPO_PATH))
