a0c44f36ac4bc5d692cdefb4b05af47ff6016c83
Jarrod McEvers <jarrodamcevers@gmail.com>
2021-12-21 15:16:07 -0600

    ssh -vT git@github.com spits out a lot of output. Hide it behind a flag.

--- lambda/main.py
+++ lambda/main.py
@@ -7,6 +7,10 @@ REPO_PATH='/tmp/{}'.format(REPO_URL.split('/')[-1].split('.')[0])
 
 secretsmanager = boto3.client('secretsmanager')
 
+def show_ssh_github():
+    print('showing what errors when sshing to github')
+    os.system('ssh -vT git@github.com')
+
 def retrieve_and_save_ssh_key():
     key = secretsmanager.get_secret_value(SecretId=os.getenv('SSH_KEY_SECRET_ID'))['SecretString']
 
@@ -17,6 +21,9 @@ def retrieve_and_save_ssh_key():
     os.chmod(ID_RSA_PATH, 0o400)
 
 def handler(event, context):
+    if os.getenv('DEBUG_SSH', None).lower() == 'true':
+        show_ssh_github()
+
     # delete everything in /tmp
     # if the lambda is still running when the next execution happens, the function will not be able to reuse the private key because of the permissions
     os.system('rm -rf /tmp/*')
@@ -24,9 +31,6 @@ def handler(event, context):
     print('printing tmp dir')
     os.system('ls /tmp'.format())
     print('\n')
-
-    # this line will fail for the lambda function that has the default ssh
-    os.system('ssh -vT git@github.com')
 
     retrieve_and_save_ssh_key()
 
