fb2088a7bfab381fb83a8a727d64b2be9e1fe3bb
Jarrod McEvers <jarrodamcevers@gmail.com>
2021-12-21 14:55:49 -0600

    Container lambda with working ssh.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,3 +1,12 @@
+data "aws_ecr_repository" "ssh_lambda" {
+  name = "ssh_lambda"
+}
+
+data "aws_ecr_image" "ssh_lambda" {
+  repository_name = "ssh_lambda"
+  image_tag       = "latest"
+}
+
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_file = "../lambda/main.py"
@@ -49,6 +58,29 @@ resource "aws_lambda_function" "working_ssh" {
       SSH_KEY_SECRET_ID = aws_secretsmanager_secret.ssh_key.id
       REPO_TO_DOWNLOAD  = "git@github.com:JarrodAMcEvers/ssh_in_aws_lambda.git"
       GIT_SSH_COMMAND   = "ssh -o StrictHostKeyChecking=no -i /tmp/id_rsa"
+    }
+  }
+}
+
+resource "aws_lambda_function" "ssh_in_container" {
+  function_name = "working_ssh_in_a_container"
+  role          = aws_iam_role.lambda.arn
+  image_uri     = "${data.aws_ecr_repository.ssh_lambda.repository_url}@${data.aws_ecr_image.ssh_lambda.image_digest}"
+  package_type  = "Image"
+  timeout       = 300
+  memory_size   = 256
+
+  vpc_config {
+    security_group_ids = [aws_security_group.ssh_lambda.id]
+    subnet_ids         = var.subnets
+  }
+
+  environment {
+    variables = {
+      REPO_TO_DOWNLOAD  = "git@github.com:JarrodAMcEvers/ssh_in_aws_lambda.git"
+      SSH_KEY_SECRET_ID = aws_secretsmanager_secret.ssh_key.id
+      # needed so that we can use the lambda layer ssh brought in
+      GIT_SSH_COMMAND = "/opt/bin/ssh -o StrictHostKeyChecking=no -i /tmp/id_rsa"
     }
   }
 }
