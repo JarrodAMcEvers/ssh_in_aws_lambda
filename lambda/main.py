9bd88f4e64cf48f1f1adf19a897129ac925c1149
Jarrod McEvers <jarrodamcevers@gmail.com>
2022-01-03 16:14:22 -0600

    Add known hosts file to get rid of ssh errors.

--- lambda/main.py
+++ lambda/main.py
@@ -11,6 +11,12 @@ def show_ssh_github():
     print('showing what errors when sshing to github')
     os.system('ssh -vT git@github.com')
 
+def create_known_hosts_file():
+    with open('/tmp/known_hosts', 'w') as file:
+        # run "ssh-keyscan -t rsa github.com" to generate this
+        file.write('github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==')
+    os.chmod('/tmp/known_hosts', 0o644)
+
 def retrieve_and_save_ssh_key():
     key = secretsmanager.get_secret_value(SecretId=os.getenv('SSH_KEY_SECRET_ID'))['SecretString']
 
@@ -28,16 +34,16 @@ def handler(event, context):
     # if the lambda is still running when the next execution happens, the function will not be able to reuse the private key because of the permissions
     os.system('rm -rf /tmp/*')
 
+    print('creating known hosts file')
+    create_known_hosts_file()
+
     print('printing tmp dir')
     os.system('ls /tmp'.format())
-    print('\n')
 
     retrieve_and_save_ssh_key()
 
     print('cloning repo')
     os.system('git clone {} {}'.format(REPO_URL, REPO_PATH))
-    print('\n')
 
     print('printing repo dir')
     os.system('ls {}'.format(REPO_PATH))
-    print('\n')
