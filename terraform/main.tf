7e6df9689769a7e345a14f4c242ec8ad126adc88
Jarrod McEvers <jarrodamcevers@gmail.com>
2022-01-13 21:43:52 -0600

    Add s3 related variables. They will be used in the new lambda function.

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,12 @@ variable "subnets" {
   type        = list(string)
   description = "List of subnets to attach to lambda functions"
 }
+
+variable "s3_bucket" {
+  type        = string
+  description = "S3 bucket name where the pem key lives"
+}
+variable "object_path" {
+  type        = string
+  description = "Object path of pem key in the S3 bucket mentioned above"
+}
